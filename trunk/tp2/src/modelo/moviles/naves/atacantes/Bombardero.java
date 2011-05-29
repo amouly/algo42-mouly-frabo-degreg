8e929b63cfd036507cc32c8408c42096d9924256
adrian@mouly.com.ar <adrian@mouly.com.ar@40e906e7-5686-c187-6c6a-b271186d2166>
2011-05-29 21:01:16 +0000

    Reconstrucción del proyecto.

---
+++ trunk/tp2/src/modelo/moviles/naves/atacantes/Bombardero.java
@@ -0,0 +1,11 @@
+package algo3.tp2.modelo.moviles.naves.atacantes;
+
+import algo3.tp2.modelo.moviles.naves.NaveAtacante;
+
+public class Bombardero extends NaveAtacante
+{
+	public Bombardero()
+	{
+		Valor = 30;
+	}
+}
---
+++ trunk/tp2/src/modelo/moviles/naves/atacantes/Caza.java
@@ -0,0 +1,11 @@
+package algo3.tp2.modelo.moviles.naves.atacantes;
+
+import algo3.tp2.modelo.moviles.naves.NaveAtacante;
+
+public class Caza extends NaveAtacante
+{
+	public Caza()
+	{
+		Valor = 30;
+	}
+}
