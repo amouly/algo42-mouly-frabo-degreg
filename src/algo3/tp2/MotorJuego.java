06590a95c6001489cc8036fc96d199b3ad75763e
matias.fra@gmail.com <matias.fra@gmail.com@40e906e7-5686-c187-6c6a-b271186d2166>
2011-06-11 22:25:17 +0000

    lower case del package atacantes, reubicacion de clases en packages

---
+++ src/algo3/tp2/MotorJuego.java
@@ -0,0 +1,43 @@
+package algo3.tp2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import algo3.tp2.modelo.moviles.naves.Nave;
+import algo3.tp2.modelo.moviles.naves.atacantes.Jugador;
+import algo3.tp2.modelo.moviles.proyectiles.Proyectil;
+import ar.uba.fi.algo3.titiritero.ControladorJuego;
+
+public class MotorJuego {
+
+	private static List<Nave> navesEnemigas = new ArrayList<Nave>();
+	private static List<Proyectil> proyectilesEnemigos = new ArrayList<Proyectil>();
+	private static List<Proyectil> proyectilesJugador = new ArrayList<Proyectil>();
+	private static Jugador jugador = new Jugador();
+	private static ControladorJuego controlador = new ControladorJuego(true);
+	
+	public static List<Nave> getNavesenemigas() {
+		return navesEnemigas;
+	}
+	public static List<Proyectil> getProyectilesenemigos() {
+		return proyectilesEnemigos;
+	}
+	public static List<Proyectil> getProyectilesjugador() {
+		return proyectilesJugador;
+	}
+	public static Jugador getJugador() {
+		return jugador;
+	}
+	
+	public void agregarProyectilEnemigo(Proyectil proyectil) {
+		proyectilesEnemigos.add(proyectil);
+		controlador.agregarObjetoVivo(proyectil);
+	}
+	
+	public static void main(String[] args) {
+		
+
+	}
+	
+	
+} 
