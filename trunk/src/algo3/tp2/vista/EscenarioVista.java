96e77c4451b0217d648bf71ae5d57bc21e632cf5
adrian@mouly.com.ar <adrian@mouly.com.ar@40e906e7-5686-c187-6c6a-b271186d2166>
2011-06-12 14:57:02 +0000

    Agregada la Vista del Escenario, y nueva ubicación para bitmaps.

---
+++ trunk/src/algo3/tp2/vista/EscenarioVista.java
@@ -0,0 +1,11 @@
+package algo3.tp2.vista;
+
+import ar.uba.fi.algo3.titiritero.vista.Imagen;
+
+public class EscenarioVista extends Imagen {
+
+	public EscenarioVista()
+	{
+		this.setNombreArchivoImagen("/res/bitmaps/Escenario.jpg");
+	}
+}
--- trunk/src/algo3/tp2/vista/JugadorVista.java
+++ trunk/src/algo3/tp2/vista/JugadorVista.java
@@ -2,12 +2,10 @@ package algo3.tp2.vista;
 
 import ar.uba.fi.algo3.titiritero.vista.Imagen;
 
-
 public class JugadorVista extends Imagen {
 
-	public JugadorVista () {
-		this.setNombreArchivoImagen("/imagenes/jugador.jpg");
+	public JugadorVista()
+	{
+		this.setNombreArchivoImagen("/res/bitmaps/Jugador.jpg");
 	}
-
-
 }
