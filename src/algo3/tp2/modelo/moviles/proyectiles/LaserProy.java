b154ac2309aee663dfbf404d18358370a947fd6a
adrian@mouly.com.ar <adrian@mouly.com.ar@40e906e7-5686-c187-6c6a-b271186d2166>
2011-06-18 15:02:26 +0000

    Corregidos algunos errores de compilación referidos a las Naves.

--- src/algo3/tp2/modelo/moviles/proyectiles/LaserProy.java
+++ src/algo3/tp2/modelo/moviles/proyectiles/LaserProy.java
@@ -2,13 +2,13 @@ package algo3.tp2.modelo.moviles.proyectiles;
 
 import java.awt.Rectangle;
 
-import algo3.tp2.modelo.vuelos.RectoVuelo;
+import algo3.tp2.modelo.vuelos.Recto;
 import ar.uba.fi.algo3.titiritero.Dibujable;
 
 public class LaserProy extends Proyectil {
 	private LaserProy(int posX, int posY) {
 		this.cuerpo = new Rectangle(posX, posY, 5, 10);
-		this.formaVuelo = new RectoVuelo(5);
+		this.formaVuelo = new Recto(5);
 	}
 	
 	public LaserProy() {
