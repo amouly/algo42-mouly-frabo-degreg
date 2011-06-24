80635b71ba57a8df1af8a6a6114ced9b90ef5d9b
adrian@mouly.com.ar <adrian@mouly.com.ar@40e906e7-5686-c187-6c6a-b271186d2166>
2011-06-24 12:53:15 +0000

    Agregada la Vista de CajaArma.

---
+++ trunk/src/algo3/tp2/vista/CajaArmaVista.java
@@ -0,0 +1,11 @@
+package algo3.tp2.vista;
+
+import ar.uba.fi.algo3.titiritero.vista.Imagen;
+
+public class CajaArmaVista extends Imagen 
+{
+	public CajaArmaVista()
+	{
+		this.setNombreArchivoImagen("/res/bitmaps/CajaArma.png");
+	}
+}
